310bedd7481bfa339c9677a4950164cf6bc30969
quake0day <quake0day@gmail.com>
2012-12-06 12:42:44 -0500

    A new beginning:) Hope the result is also good:)

---
+++ Onix/Onix/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Onix
+//
+//  Created by quake0day on 12/6/12.
+//  Copyright (c) 2012 quake0day. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
